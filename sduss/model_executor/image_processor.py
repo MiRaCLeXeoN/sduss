0139d09a692dcb9a9422b2b63d27207540b7d124
MiRaCLeXeoN <zzp905020708@gmail.com>
2024-03-16 22:15:31 +0800

    SD models integration start

---
+++ sduss/model_executor/image_processor.py
@@ -0,0 +1,15 @@
+
+from typing import Union, List
+
+import numpy as np
+import PIL.Image
+import torch
+
+PipelineImageInput = Union[
+    PIL.Image.Image,
+    np.ndarray,
+    torch.FloatTensor,
+    List[PIL.Image.Image],
+    List[np.ndarray],
+    List[torch.FloatTensor],
+]
---
+++ sduss/model_executor/pipelines/stable_diffusion/pipeline_stable_diffusion.py
@@ -0,0 +1,42 @@
+
+from typing import overload, Union, Optional, List, Dict, Any, Callable
+
+import torch
+
+from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion import (
+    StableDiffusionPipeline as DiffusersStableDiffusionPipeline)
+
+from sduss.model_executor.image_processor import PipelineImageInput
+
+class StableDiffusionPipeline(DiffusersStableDiffusionPipeline):
+    
+    @overload
+    @torch.no_grad
+    def __call__(
+        self,
+        prompt: Union[str, List[str]] = None,
+        height: Optional[int] = None,
+        width: Optional[int] = None,
+        num_inference_steps: int = 50,
+        timesteps: List[int] = None,
+        guidance_scale: float = 7.5,
+        negative_prompt: Optional[Union[str, List[str]]] = None,
+        num_images_per_prompt: Optional[int] = 1,
+        eta: float = 0.0,
+        generator: Optional[Union[torch.Generator, List[torch.Generator]]] = None,
+        latents: Optional[torch.FloatTensor] = None,
+        prompt_embeds: Optional[torch.FloatTensor] = None,
+        negative_prompt_embeds: Optional[torch.FloatTensor] = None,
+        ip_adapter_image: Optional[PipelineImageInput] = None,
+        ip_adapter_image_embeds: Optional[List[torch.FloatTensor]] = None,
+        output_type: Optional[str] = "pil",
+        return_dict: bool = True,
+        cross_attention_kwargs: Optional[Dict[str, Any]] = None,
+        guidance_rescale: float = 0.0,
+        clip_skip: Optional[int] = None,
+        callback_on_step_end: Optional[Callable[[int, int, Dict], None]] = None,
+        callback_on_step_end_tensor_inputs: List[str] = ["latents"],
+        **kwargs
+    ):
+        
+
