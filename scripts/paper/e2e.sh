9411dc3ab13e21cbcab37af860a5c7a10f8673c2
MiracleXeon <zzp905020708@gmail.com>
2025-11-25 02:12:29 +0000

    feat: add scripts

---
+++ scripts/paper/e2e.sh
@@ -0,0 +1,92 @@
+set -e
+
+# For figure 12
+
+MODEL_LIST=("sdxl" "sd3")
+DP_LIST="1"
+
+for model in "${MODEL_LIST[@]}"; do
+    if [[ $model == "sd3" ]]; then
+        QPS_LIST=(0.1 0.2 0.3 0.4 0.5)
+        POLICY_LIST=("esymred" "fcfs_mixed" "orca_resbyres")
+    elif [[ $model == "sdxl" ]]; then
+        QPS_LIST=(0.8 0.9 1.0 1.1 1.2)
+        POLICY_LIST=("esymred" "fcfs_mixed" "orca_resbyres")
+    fi
+
+    export SLO="5"
+    export MODEL=$model
+    export NUM=$NUM
+
+    for dp_size in $DP_LIST; do
+        for policy_name in $POLICY_LIST; do 
+            for qps in $QPS_LIST; do
+                export QPS=$(awk "BEGIN {printf \"%.1f\", $qps * $dp_size}")
+                export POLICY=$policy_name
+                export DATA_PARALLEL_SIZE=$dp_size
+                # last_gpu=$(awk "BEGIN {printf $dp_size - 1}")
+                # export GPUS="[0-${last_gpu}]"
+                start_gpu=$(awk "BEGIN {printf 8 - $dp_size}")
+                export GPUS="[${start_gpu}-7]"
+
+                if [[ $POLICY == "esymred" || $POLICY == "fcfs_mixed" ]]; then
+                    export USE_MIXED_PRECISION="--use_mixed_precision"
+                    export ESYMRED_USE_CACHE="TRUE"
+                elif [[ $POLICY == "orca_resbyres" ]]; then
+                    export USE_MIXED_PRECISION=""
+                    export ESYMRED_USE_CACHE="FALSE"
+                else
+                    export USE_MIXED_PRECISION=""
+                fi
+
+                result_dir_path="./results/${MODEL}/${QPS}_${SLO}_${POLICY}_${DATA_PARALLEL_SIZE}"
+                if [ -d "${result_dir_path}" ]; then
+                    find ${result_dir_path} -type f -delete
+                else
+                    mkdir -p "${result_dir_path}"
+                fi
+
+                export SDUSS_COLLECT_DATA=true
+                export ESYMRED_PREDICTOR_PATH="./exp/schedule_predictor_$MODEL.pkl"
+                export ESYMRED_EXEC_TIME_DIR="./exp/profile"
+
+                export ESYMRED_UPSAMPLE_PATH="./exp/$MODEL-upsample-threshold0.01.pkl"
+                export ESYMRED_DOWNSAMPLE_PATH="./exp/$MODEL-downsample-threshold0.01.pkl"
+                export ESYMRED_TRANSFORMER_PATH="./exp/$MODEL-state-threshold0.01.pkl"
+
+                if [ ${MODEL} == "sd1.5" ]; then
+                    export MODEL_PATH=$SD15_PATH
+                elif [ ${MODEL} == "sdxl" ]; then
+                    export MODEL_PATH=$SDXL_PATH
+                elif [ ${MODEL} == "sd3" ]; then
+                    export MODEL_PATH=$SD3_PATH
+                fi
+
+                echo "Start ${result_dir_path}"
+
+                python ./tests/server/direct_test.py \
+                    --model_name_or_pth ${MODEL_PATH} \
+                    --policy ${POLICY} \
+                    --dispatcher_policy greedy \
+                    ${USE_MIXED_PRECISION} \
+                    --use_esymred \
+                    --max_batchsize 12 \
+                    --torch_dtype "float16" \
+                    --engine_use_mp \
+                    --worker_use_mp \
+                    --data_parallel_size ${DATA_PARALLEL_SIZE} \
+                    --gpus "${GPUS}" \
+                    --model ${MODEL} \
+                    --qps ${QPS} \
+                    --SLO ${SLO} \
+                    --host localhost \
+                    --port 8000 \
+                    --num ${NUM} > "${result_dir_path}/unit_test.log" 2>&1
+
+                # Copy logs
+                cp ./outputs/gpu_worker_* "${result_dir_path}/"
+                rm -f ./outputs/gpu_worker_*
+            done
+        done
+    done
+done 
---
+++ scripts/paper/run_all.sh
@@ -0,0 +1,32 @@
+# ----------------------------------
+# To run the test successfully, you MUST update the following variables
+# ----------------------------------
+export TORCH_INCLUDE_PATH="/root/miniconda3/envs/sduss/lib/python3.9/site-packages/torch/include"
+
+# 2. specify the model path.
+# We use the direct path of the model weights downloaded from Hugging Face.
+# The `workspace` folder is mounted from the host machine inside the docker container.
+# If you choose to download the model weights inside the docker container,
+# please make sure to update the following paths accordingly.
+# Take the following paths as examples.
+export SD15_PATH="/workspace/huggingface/hub/models--sd-legacy--stable-diffusion-v1-5/snapshots/f03de327dd89b501a01da37fc5240cf4fdba85a1"
+export SDXL_PATH="/workspace/huggingface/hub/models--stabilityai--stable-diffusion-xl-base-1.0/snapshots/462165984030d82259a11f4367a4eed129e94a7b"
+export SD3_PATH="/workspace/huggingface/hub/models--stabilityai--stable-diffusion-3.5-medium/snapshots/b940f670f0eda2d07fbb75229e779da1ad11eb80"
+
+# 3. This variable controls the number of requests to be sent in each experiment.
+# We use 500 for default settings. But this would cause the experiments to run for a long time.
+# 500 requests may take around 20-30 hours to finish all experiments.
+# The smaller the number is, the faster the experiments finish.
+export NUM="500"
+
+# figure 12
+bash ./scripts/paper/e2e.sh
+
+# figure 13
+bash ./scripts/paper/distribution.sh
+
+# figure 14
+bash ./scripts/paper/scalibility.sh
+
+# figure 15
+bash ./scripts/paper/SLO.sh
